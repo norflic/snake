3468638dc3675cfdf9da05f61bdb77154b23f56a
nils <nils.derrien@gmail.com>
2024-12-20 14:37:19 +0100

    tp works

--- Bonus.py
+++ Bonus.py
@@ -1,31 +1,38 @@
 import pygame
 
 
-# def check_tp_pos(game, margin, pos_x, pos_y):
-#
-#
-#
-#             tp(game)
-#
-# def check_queue_and_apple(game):
-#     for queue in game.snake:
-#         if queue.rect.x != game.snake.rect.x or queue.rect.y != game.snake.rect.y:
-#             return True
-#         else :
-#             return False
-#
-# def check_x(game, pos_margin, pos_x):
-#     return (pos_x > pos_margin and pos_x < game.SCREEN_WIDTH - pos_margin)
-#
-# def check_y(game, pos_margin, pos_y):
-#     return(pos_y > pos_margin and pos_y < game.SCREEN_HEIGHT - pos_margin)
-#
-# def tp(game):
-#     pos_margin = game.length_unit * 2
-#     pos_x, pos_y = check_tp_pos()
-#
-#     game.snake.rect.x = posx
-#     game.snake.rect.y = posy
+def check_tp_pos(game, margin, pos_x, pos_y):
+    if check_queue_and_apple(game, pos_x, pos_y):
+        if check_x(game, margin, pos_x):
+            if check_y(game, margin, pos_y):
+                print(f"posx: {pos_x}, posy: {pos_y}")
+                return pos_x, pos_y
+            else:
+                print(f" le y est pas bon {pos_y}")
+        else:
+            print(f" le x est pas bon {pos_x}")
+    else:
+        print(f" coll avec queue")
+    pos_x, pos_y = game.get_random_tiled_pos()
+    return check_tp_pos(game, margin, pos_x, pos_y)
+
+def check_queue_and_apple(game, pos_x, pos_y):
+    for queue in game.snake.tail_list:
+        if queue.rect.x != pos_x or queue.rect.y != pos_y:
+            return True
+        else :
+            return False
+def check_x(game, pos_margin, pos_x):
+    return (pos_x > pos_margin and pos_x < game.SCREEN_WIDTH - pos_margin)
+def check_y(game, pos_margin, pos_y):
+    return(pos_y > pos_margin and pos_y < game.SCREEN_HEIGHT - pos_margin)
+
+def tp(game):
+    pos_margin = game.length_unit * 2
+    pos_x, pos_y = game.get_random_tiled_pos()
+    pos_x, pos_y = check_tp_pos(game, pos_margin, pos_x, pos_y)
+    game.snake.rect.x = pos_x
+    game.snake.rect.y = pos_y
 
 
 
